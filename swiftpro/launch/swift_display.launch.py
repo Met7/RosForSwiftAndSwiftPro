f971958e97fd583efb0ebf68636837d32b8d6478
Met <dominik.franek@gmail.com>
2020-07-29 05:54:18 -0400

    more swiftpro launch files updated to python

---
+++ swiftpro/launch/swift_display.launch.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2020 Open Source Robotics Foundation, Inc.
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# This code is derived from original robot state publisher example.
+
+import os
+import launch
+import launch_ros.actions
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import xacro
+
+def generate_launch_description():
+    nodes = [
+      Node(
+        package='swiftpro',
+        node_executable='swiftpro_rviz_node',
+        node_name='swiftpro_rviz_node'
+      ),
+      Node(
+        package='swiftpro',
+        node_executable='swiftpro_read_node',
+        node_name='swiftpro_read_node'
+      ),
+      Node(
+        package='rviz2',
+        node_executable='rviz2',
+        node_name='rviz2'
+      ),
+    ]
+    
+    pkg_share = FindPackageShare('swiftpro').find('swiftpro')
+    urdf_dir = os.path.join(pkg_share, 'urdf')
+    xacro_file = os.path.join(urdf_dir, 'swift_model.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_desc = doc.toprettyxml(indent='  ')
+    params = {'robot_description': robot_desc}
+    rsp = Node(package='robot_state_publisher',
+               node_executable='robot_state_publisher',
+               output='both',
+               parameters=[params])
+
+    nodes.append(rsp)
+    return launch.LaunchDescription(nodes)
